ba4f0eb0fb58fb2a73b6f2667411aea1ee4754e1
Jordan <mitchumjordan11@gmail.com>
2023-05-25 14:01:00 -0700

    A.F added to check for when logged in

--- webapp.py
+++ webapp.py
@@ -103,7 +103,7 @@ def authorized():
             #pprint.pprint(vars(github['api/2/accounts/profile/']))
             cart = collection.find_one({'User': session['user_data']['id']})
             if cart is None:
-                collection.insert_one({'User':session['user_data']['id'], 'Item-Name':[]})
+                collection.insert_one({'User':session['user_data']['id'], 'Item-Name':[], 'AF': False})
             flash('You were successfully logged in as ' + session['user_data']['login'] + '.')
         except Exception as inst:
             session.clear()
@@ -161,9 +161,10 @@ def fish():
 @app.route('/eyes', methods=["GET", "POST"])
 def eyes():
     return render_template('eyes.html')
-
-
-
+    
+'''
+End of Items
+'''
     
 @app.route('/addtoCart', methods=["GET", "POST"])
 def addtoCart():
